43ed7967057770f7ccf095b474e3de85f5251635
Josip Miskovic <josipmiskovic@gmail.com>
2019-05-20 17:40:50 +0200

    Service worker for offline caching

---
+++ serviceworker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/figments-site/',
+       '/figments-site/index.html',
+       '/figments-site/icon32.png',
+       '/figments-site/icon512.png',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
